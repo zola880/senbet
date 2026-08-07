cfa5ae8ac76ae4d730ae928c00dfd3bfe7f95fd7
zelalem yibabe <zelalemybabe77@gmail.com>
2026-08-08 01:57:05 +0300

    Refactor CSS: Modularize component styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
+import './global.css';
+import './responsive.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
